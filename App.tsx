7e533e1c8999c0aff2e3d6ee81a4fd639ec20ed2
Christian Kirschberg <ckirschberg@gmail.com>
2024-04-04 11:04:42 +0200

    .

--- App.tsx
+++ App.tsx
@@ -1,15 +1,8 @@
 
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
 import { store } from './store/store'
 import { Provider } from 'react-redux'
-import { Counter } from './components/counter';
-import { Categories } from './screens/Categories';
-import SignupScreen from './screens/SignupScreen';
-import LoginScreen from './screens/LoginScreen';
 import MainNavigation from './screens/MainNavigation';
-
 
 export default function App() {
   return (
